149200e0d6686c2f833a8f091cbe1261c9094dcc
ToanNguyen <toanntph20633@fpt.edu.vn>
2023-09-01 22:48:54 +0700

    Initial commit

---
+++ src/test/java/com/example/fashionmanager/FashionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fashionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
