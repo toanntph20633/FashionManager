a1320cfe6eee40e75567362d3e8fffa25d176565
ToanNguyen <130726778+toan12450@users.noreply.github.com>
2023-10-12 13:55:02 +0700

    change entity

--- src/main/java/com/example/fashionmanager/entity/LichSuTuongTacSanPhamEntity.java
+++ src/main/java/com/example/fashionmanager/entity/LichSuTuongTacSanPhamEntity.java
@@ -1,38 +1,38 @@
-package com.example.fashionmanager.entity;
-
-import com.example.fashionmanager.entity.common.CommonEntity;
-import com.example.fashionmanager.enums.HistoryMethod;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
-import java.io.Serializable;
-
-@Entity
-@Table(name = "lich_su_tuong_tac_san_pham_entity")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@SuperBuilder
-public class LichSuTuongTacSanPhamEntity extends CommonEntity implements Serializable {
-    @ManyToOne
-    @JoinColumn(name = "san_pham_id")
-    private SanPhamEntity sanPhamEntity;
-    @ManyToOne
-    @JoinColumn(name = "nhan_vien_id")
-    private NhanVienEntity nhanVienEntity;
-
-    @Column(name = "thao_tac")
-    @Enumerated(EnumType.STRING)
-    private HistoryMethod thaoTac;
-}
+//package com.example.fashionmanager.entity;
+//
+//import com.example.fashionmanager.entity.common.CommonEntity;
+//import com.example.fashionmanager.enums.HistoryMethod;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.EnumType;
+//import jakarta.persistence.Enumerated;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.experimental.SuperBuilder;
+//
+//import java.io.Serializable;
+//
+//@Entity
+//@Table(name = "lich_su_tuong_tac_san_pham_entity")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@SuperBuilder
+//public class LichSuTuongTacSanPhamEntity extends CommonEntity implements Serializable {
+//    @ManyToOne
+//    @JoinColumn(name = "san_pham_id")
+//    private SanPhamEntity sanPhamEntity;
+//    @ManyToOne
+//    @JoinColumn(name = "nhan_vien_id")
+//    private NhanVienEntity nhanVienEntity;
+//
+//    @Column(name = "thao_tac")
+//    @Enumerated(EnumType.STRING)
+//    private HistoryMethod thaoTac;
+//}
--- src/main/java/com/example/fashionmanager/entity/MaGiamGiaEntity.java
+++ src/main/java/com/example/fashionmanager/entity/MaGiamGiaEntity.java
@@ -1,43 +1,43 @@
-package com.example.fashionmanager.entity;
-
-import com.example.fashionmanager.entity.common.CommonEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "voucher_entity")
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@SuperBuilder
-public class MaGiamGiaEntity extends CommonEntity {
-    @Column(name = "ma_voucher")
-    private String maVoucher;
-
-    @Column(name = "ngay_bat_dau")
-    private LocalDate ngayBatDau;
-
-    @Column(name = "ngay_ket_thuc")
-    private LocalDate ngayKetThuc;
-
-    @Column(name = "tieu_de")
-    private String tieuDe;
-
-    @Column(name = "mo_ta")
-    private String mota;
-
-    @Column(name = "so_tien_yeu_cau")
-    private BigDecimal soTienYeuCau;
-
-
-}
+//package com.example.fashionmanager.entity;
+//
+//import com.example.fashionmanager.entity.common.CommonEntity;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.experimental.SuperBuilder;
+//
+//import java.math.BigDecimal;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = "voucher_entity")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@SuperBuilder
+//public class MaGiamGiaEntity extends CommonEntity {
+//    @Column(name = "ma_voucher")
+//    private String maVoucher;
+//
+//    @Column(name = "ngay_bat_dau")
+//    private LocalDate ngayBatDau;
+//
+//    @Column(name = "ngay_ket_thuc")
+//    private LocalDate ngayKetThuc;
+//
+//    @Column(name = "tieu_de")
+//    private String tieuDe;
+//
+//    @Column(name = "mo_ta")
+//    private String mota;
+//
+//    @Column(name = "so_tien_yeu_cau")
+//    private BigDecimal soTienYeuCau;
+//
+//
+//}
