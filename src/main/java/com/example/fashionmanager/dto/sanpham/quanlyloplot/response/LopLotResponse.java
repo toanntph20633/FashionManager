37c91ab438291105557a49cce1875cbf3412a23f
ToanNguyen <130726778+toan12450@users.noreply.github.com>
2023-10-17 10:35:26 +0700

    update code CRUD Lop lot

---
+++ src/main/java/com/example/fashionmanager/dto/sanpham/quanlyloplot/response/LopLotResponse.java
@@ -0,0 +1,13 @@
+package com.example.fashionmanager.dto.sanpham.quanlyloplot.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LopLotResponse {
+    private Long id;
+    private String tenLopLot;
+}
