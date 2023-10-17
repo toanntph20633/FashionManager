edcd54c586982f2202e0e03a55a23529299f9bc8
MacorZNguyen <130726778+toan12450@users.noreply.github.com>
2023-10-17 16:32:53 +0700

    update init database

--- src/main/java/com/example/fashionmanager/service/InitDatabaseService.java
+++ src/main/java/com/example/fashionmanager/service/InitDatabaseService.java
@@ -2,4 +2,19 @@ package com.example.fashionmanager.service;
 
 public interface InitDatabaseService {
     void initData();
+     void initKieuDang();
+     void initXeTa();
+     void initKieuDet();
+     void initKieuTui();
+     void initDanhMuc();
+
+     void initChatLieu();
+     void initSanPham();
+     void initMauSac();
+     void initKichThuoc();
+     void initCauTrucKhuy();
+     void  initBinhLuan();
+     void initLopLot();
+     void initHoaTiet();
+     void initVeAo();
 }
