b00114376cdf7f38f6f3d367ff2d89163889765d
AnHieu002 <hieuavph20284@fpt.edu.vn>
2023-10-30 14:36:47 +0700

    update  controller

--- src/main/java/com/example/fashionmanager/controller/admin/MaGiamGia/MaGiamGiaController.java
+++ src/main/java/com/example/fashionmanager/controller/admin/MaGiamGia/MaGiamGiaController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin/ma-giam-gia")
+@CrossOrigin(origins = "*", maxAge = -1)
 public class MaGiamGiaController {
     @Autowired
     MaGiamGiaServiceImpl maGiamGiaService;
